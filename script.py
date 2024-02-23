ecdd7b1cc060852e7eeaef6565783ea0cad12de6
Omprakash Verma <omprakash_verma@persistent.com>
2024-02-23 15:24:02 +0530

    Updated packages for generating docstring

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import shutil
 
+# Destination Python repository path
 destination_repo = input("Please enter the repository path: ")
 
 # Install Sphinx 5.2.2 using pip
@@ -12,22 +13,19 @@ subprocess.run(["pip", "install", "Sphinx==5.2.2"])
 # Source folder path
 docs_source_folder = 'docstring-package/docs'
 
-# Destination Python repository path
-#destination_repo = 'library-management-system'
+
 
 # Copy source folder to Python repository
 shutil.copytree(docs_source_folder, destination_repo,dirs_exist_ok=True)
 
-# Docstring script
+# Copy docstring script
 docs_script_folder = 'docstring-package/docstringscript'
 destination_dir = os.getcwd()
-
 shutil.copytree(docs_script_folder, destination_dir ,dirs_exist_ok=True)
 
 
-
+# Execute docstring script
 python_file_path = "run_docstring.py"
-
 subprocess.run(["python", python_file_path,destination_repo])
 
 # Change directory to the Python repository
